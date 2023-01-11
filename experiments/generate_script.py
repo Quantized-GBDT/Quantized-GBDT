405d5f7d395e37f28632643795d876325c6c5b9b
Yu Shi <shiyu_k1994@qq.com>
2023-01-11 06:17:09 +0000

    add xgboost configuration

--- experiments/generate_script.py
+++ experiments/generate_script.py
@@ -9,6 +9,7 @@ arg_parser.add_argument("--stochastic-rounding", action='store_true')
 arg_parser.add_argument("--for-speed", action='store_true')
 arg_parser.add_argument("--device", type=str, default='cpu')
 arg_parser.add_argument("--force-col-wise", action='store_true')
+arg_parser.add_argument("--algorithm", type=str, default='lgb')
 
 script_fname = 'run.sh'
 running = open(script_fname, 'w')
@@ -40,8 +41,8 @@ col_wise_data = ['epsilon', 'year', 'yahoo', 'bosch']
 
 bins = [2, 3, 4, 5]
 
-def generate_script(data_path, use_discretized_grad, discretized_grad_renew, stochastic_rounding, for_speed, device):
-
+def generate_script(data_path, use_discretized_grad, discretized_grad_renew, stochastic_rounding, for_speed, device, algorithm):
+    data_path = data_path.rstrip('/')
     dataset = [
         f'data={data_path}/higgs.train',
         f'data={data_path}/epsilon.train',
@@ -65,33 +66,55 @@ def generate_script(data_path, use_discretized_grad, discretized_grad_renew, sto
     ]
 
     os.system("mkdir -p logs")
-    use_discretized_grad_str = str(use_discretized_grad).lower()
-    discretized_grad_renew_str = str(discretized_grad_renew).lower()
-    stochastic_rounding_str = str(stochastic_rounding).lower()
-    num_k = 4 if use_discretized_grad else 1
-    for i in range(8):
-        for j in range(5):
-            for k in range(num_k):
-                base_conf_fname = 'train_model.conf' if task[i] == 'binary' else ('train_rank_model.conf' if task[i] == 'ranking' else 'train_reg_model.conf')
+
+    if algorithm == 'lgb':
+        use_discretized_grad_str = str(use_discretized_grad).lower()
+        discretized_grad_renew_str = str(discretized_grad_renew).lower()
+        stochastic_rounding_str = str(stochastic_rounding).lower()
+        num_k = 4 if use_discretized_grad else 1
+        for i in range(8):
+            for j in range(5):
+                for k in range(num_k):
+                    base_conf_fname = 'train_model.conf' if task[i] == 'binary' else ('train_rank_model.conf' if task[i] == 'ranking' else 'train_reg_model.conf')
+                    args = ''
+                    args += dataset[i]
+                    if not for_speed:
+                        args += ' ' + validset[i]
+                    args += ' seed=' + str(j)
+                    if use_discretized_grad:
+                        args += ' grad_discretize_bins='+str(2**bins[k]-2)
+                        log_name = './logs/train_' + data[i] + '_seed'+str(j) + '_bins' + str(bins[k])+'.log'
+                    else:
+                        log_name = './logs/train_' + data[i] + '_seed'+str(j)+ '_fp32' + '.log'
+                    args += f' use_discretized_grad={use_discretized_grad_str} discretized_grad_renew={discretized_grad_renew_str} stochastic_rounding={stochastic_rounding_str}'
+                    if data[i] == 'bosch':
+                        args += ' learning_rate=0.015'
+                    if data[i] in col_wise_data:
+                        args += ' force_row_wise=false force_col_wise=true'
+                    if device != 'cpu':
+                        args += f' device_type={device} gpu_device_id=0'
+                    running.write(f'../LightGBM/lightgbm config={base_conf_fname} {args} > {log_name} 2>&1\n')
+    elif algorithm == 'xgb':
+        for i in range(8):
+            for j in range(5):
+                log_name = './logs/train_' + data[i] + '_seed'+str(j)+ '_xgb' + '.log'
+                base_conf_fname = 'xgboost.conf'
                 args = ''
                 args += dataset[i]
-                if not for_speed:
-                    args += ' ' + validset[i]
                 args += ' seed=' + str(j)
-                if use_discretized_grad:
-                    args += ' grad_discretize_bins='+str(2**bins[k]-2)
-                    log_name = './logs/train_' + data[i] + '_seed'+str(j) + '_bins' + str(bins[k])+'.log'
-                else:
-                    log_name = './logs/train_' + data[i] + '_seed'+str(j)+ '_fp32' + '.log'
-                args += f' use_discretized_grad={use_discretized_grad_str} discretized_grad_renew={discretized_grad_renew_str} stochastic_rounding={stochastic_rounding_str}'
+                if not for_speed:
+                    args += ' ' + validset[i].replace('valid=', 'eval[test]=')
+                    metric = 'auc' if task[i] == 'binary' else ('rmse' if task[i] == 'regression' else 'ndcg@10')
+                    args += f' eval_metric={metric}'
+                objective = 'binary:logistic' if task[i] == 'binary' else ('reg:linear' if task[i] == 'regression' else 'rank:pairwise')
+                args += f' objective={objective}'
                 if data[i] == 'bosch':
-                    args += ' learning_rate=0.015'
-                if data[i] in col_wise_data:
-                    args += ' force_row_wise=false force_col_wise=true'
-                if device != 'cpu':
-                    args += f' device_type={device} gpu_device_id=0'
-                running.write(f'../LightGBM/lightgbm config={base_conf_fname} {args} > {log_name} 2>&1\n')
+                    args += ' eta=0.015 max_leaves=45' # max_leaves=45 for xgboost to reduce time cost for post pruning
+                if device == 'cuda':
+                    args += ' tree_method=gpu_hist'
+                running.write(f'../xgboost/xgboost {base_conf_fname} {args} > {log_name} 2>&1\n')
+    
 
 if __name__ == '__main__':
     args = arg_parser.parse_args()
-    generate_script(args.data_path, args.use_discretized_grad, args.discretized_grad_renew, args.stochastic_rounding, args.for_speed, args.device)
+    generate_script(args.data_path, args.use_discretized_grad, args.discretized_grad_renew, args.stochastic_rounding, args.for_speed, args.device, args.algorithm)
