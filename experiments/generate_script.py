855353dfedde0d152c869d9749b800575d5c306a
Yu Shi <shiyu_k1994@qq.com>
2023-01-17 06:57:20 +0000

    fix ranking for xgboost

--- experiments/generate_script.py
+++ experiments/generate_script.py
@@ -65,7 +65,9 @@ def generate_script(data_path, use_discretized_grad, discretized_grad_renew, sto
         f'valid={data_path}/year.test'
     ]
 
-    os.system("mkdir -p logs")
+    log_dir = 'logs' if not for_speed else 'logs_for_speed'
+
+    os.system(f"mkdir -p {log_dir}")
 
     if algorithm == 'lgb':
         use_discretized_grad_str = str(use_discretized_grad).lower()
@@ -83,56 +85,63 @@ def generate_script(data_path, use_discretized_grad, discretized_grad_renew, sto
                     args += ' seed=' + str(j)
                     if use_discretized_grad:
                         args += ' grad_discretize_bins='+str(2**bins[k]-2)
-                        log_name = './logs/train_' + data[i] + '_seed'+str(j) + '_bins' + str(bins[k])+'.log'
+                        log_name = f'./{log_dir}/train_' + data[i] + '_seed'+str(j) + '_bins' + str(bins[k])+'.log'
                     else:
-                        log_name = './logs/train_' + data[i] + '_seed'+str(j)+ '_fp32' + '.log'
+                        log_name = f'./{log_dir}/train_' + data[i] + '_seed'+str(j)+ '_fp32' + '.log'
                     args += f' use_discretized_grad={use_discretized_grad_str} discretized_grad_renew={discretized_grad_renew_str} stochastic_rounding={stochastic_rounding_str}'
                     if data[i] == 'bosch':
-                        args += ' learning_rate=0.015'
+                        args += ' learning_rate=0.015 num_leaves=45'
                     if data[i] in col_wise_data:
                         args += ' force_row_wise=false force_col_wise=true'
                     if device != 'cpu':
-                        args += f' device_type={device} gpu_device_id=0'
+                        args += f' device_type=cuda gpu_device_id=0'
                     running.write(f'../LightGBM/lightgbm config={base_conf_fname} {args} > {log_name} 2>&1\n')
     elif algorithm == 'xgb':
         for i in range(8):
             for j in range(5):
-                log_name = './logs/train_' + data[i] + '_seed'+str(j)+ '_xgb' + '.log'
+                log_name = f'./{log_dir}/train_' + data[i] + '_seed'+str(j)+ '_xgb' + '.log'
                 base_conf_fname = 'xgboost.conf'
                 args = ''
                 args += dataset[i]
+                if task[i] == 'ranking':
+                    args += '.xgb?format=libsvm'
                 args += ' seed=' + str(j)
                 if not for_speed:
                     args += ' ' + validset[i].replace('valid=', 'eval[test]=')
+                    if task[i] == 'ranking':
+                        args += '.xgb?format=libsvm'
                     metric = 'auc' if task[i] == 'binary' else ('rmse' if task[i] == 'regression' else 'ndcg@10')
                     args += f' eval_metric={metric}'
                 objective = 'binary:logistic' if task[i] == 'binary' else ('reg:linear' if task[i] == 'regression' else 'rank:pairwise')
                 args += f' objective={objective}'
                 if data[i] == 'bosch':
                     args += ' eta=0.015 max_leaves=45' # max_leaves=45 for xgboost to reduce time cost for post pruning
-                if device == 'cuda':
+                if device != 'cpu':
                     args += ' tree_method=gpu_hist'
                 running.write(f'../xgboost/xgboost {base_conf_fname} {args} > {log_name} 2>&1\n')
     elif algorithm == 'cat':
         for i in range(8):
             for j in range(5):
-                log_name = './logs/train_' + data[i] + '_seed'+str(j)+ '_cat' + '.log'
+                log_name = f'./{log_dir}/train_' + data[i] + '_seed'+str(j)+ '_cat' + '.log'
                 base_conf_fname = 'catboost.json'
                 args = ''
                 args += f"--params-file {base_conf_fname}"
                 if data[i] == 'bosch':
-                    args += " --learning-rate 0.015"
-                data_path_prefix = 'libsvm://' if task[i] != 'rank' else ''
-                data_path_suffix = '' if task[i] != 'rank' else '.cat'
+                    args += " --learning-rate 0.015 --max-leaves 45"
+                data_path_prefix = 'libsvm://' if task[i] != 'ranking' else ''
+                data_path_suffix = '' if task[i] != 'ranking' else '.cat'
                 data_path_for_catboost = dataset[i].split('=')[-1]
                 args += f" --learn-set {data_path_prefix}{data_path_for_catboost}{data_path_suffix}"
-                valid_path_for_catboost = validset[i].split('=')[-1]
-                args += f" --test-set {data_path_prefix}{valid_path_for_catboost}{data_path_suffix}"
+                if not for_speed:
+                    valid_path_for_catboost = validset[i].split('=')[-1]
+                    args += f" --test-set {data_path_prefix}{valid_path_for_catboost}{data_path_suffix}"
                 args += f" --column-description {data_path_for_catboost.split('.')[0]}.cd"
                 loss_function = "Logloss" if task[i] == 'binary' else ("RMSE" if task[i] == 'regression' else "YetiRank")
                 args += f" --loss-function {loss_function}"
-                eval_metric = "AUC" if task[i] == 'binary' else ("RMSE" if task[i] == 'regression' else "NDCG:top=10\\;type=Exp")
-                args += f" --eval-metric {eval_metric}"
+                if not for_speed:
+                    eval_metric = "AUC" if task[i] == 'binary' else ("RMSE" if task[i] == 'regression' else "NDCG:top=10\\;type=Exp")
+                    args += f" --eval-metric {eval_metric}"
+                    args += " --metric-period 1"
                 task_type = "CPU" if device == 'cpu' else "GPU --devices 0"
                 args += f" --task-type {task_type}"
                 args += f" --random-seed {j}"
